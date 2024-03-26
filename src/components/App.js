c81e711f8e8e274fbdeba7efb730708019b72567
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-03-26 12:11:39 +0530

    implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      <button onClick={handleClick}>Click me</button>
+      <p>Button clicked {count} times</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
